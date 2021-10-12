467d002d3e90ee5d290976fef67cf3eaf25e6ee1
Sujit Nangare <82749991+sujit6060@users.noreply.github.com>
2021-10-12 11:10:12 +0530

    updating project

--- src/WeatherApp/Weather.js
+++ src/WeatherApp/Weather.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import './style.css'
 
 export const Weather = () => {
     const [city, setCity] = useState(null);
@@ -17,7 +18,7 @@ export const Weather = () => {
             <br></br>
             <br></br>
             <div>
-                <div className="container" style={{ textAlign: "center", color: "white", border: "2px solid #e5e4e2", borderRadius: "17px", width: "300px", height: "470px", backgroundImage: `url("https://images.unsplash.com/photo-1603411951734-23ae414fe112?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmxhY2slMjBuYXR1cmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")` }}>
+                <div className="container" style={{ textAlign: "center", color: "white", border: "2px solid #e5e4e2", borderRadius: "17px", width: "320px", height: "500px", backgroundImage: `url("https://images.unsplash.com/photo-1603411951734-23ae414fe112?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmxhY2slMjBuYXR1cmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")` }}>
                     <div>
                         <br></br>
                         <input style={{ padding: "15px", borderRadius: "20px" }} type="search" onChange={(event) => { setSearch(event.target.value) }} placeholder="Search City Here..."></input>
